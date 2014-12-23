7c5b4a6bcbfdeb2080fb6a78f691a38303f1a9c9
landhero <raindrops_fall@sina.com>
2014-12-23 16:42:50 +0800

    Add Test Framework

---
+++ LifeTicker/LifeTickerTest/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+namespace LifeTickerTest
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData(1,"1")]
+        public void PassingTest(int a, string b) {
+            Assert.Equal(a, (int)Int32.Parse(b));
+        }
+
+        [Fact]
+        public void anthTest() {
+            Assert.Equal(1, 1);
+        }
+    }
+}
